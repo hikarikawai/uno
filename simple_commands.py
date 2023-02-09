57397b301861810acc15d924d4c60b53c48829ee
HIKARI <121547505+hikarikawai@users.noreply.github.com>
2023-02-09 14:32:57 +0700

    tes indo

--- simple_commands.py
+++ simple_commands.py
@@ -28,41 +28,30 @@ from internationalization import _, user_locale
 @user_locale
 def help_handler(update: Update, context: CallbackContext):
     """Handler for the /help command"""
-    help_text = _("Follow these steps:\n\n"
-      "1. Add this bot to a group\n"
-      "2. In the group, start a new game with /new or join an already"
-      " running game with /join\n"
-      "3. After at least two players have joined, start the game with"
-      " /start\n"
-      "4. Type <code>@unobot</code> into your chat box and hit "
-      "<b>space</b>, or click the <code>via @unobot</code> text "
-      "next to messages. You will see your cards (some greyed out), "
-      "any extra options like drawing, and a <b>?</b> to see the "
-      "current game state. The <b>greyed out cards</b> are those you "
-      "<b>can not play</b> at the moment. Tap an option to execute "
-      "the selected action.\n"
-      "Players can join the game at any time. To leave a game, "
-      "use /leave. If a player takes more than 90 seconds to play, "
-      "you can use /skip to skip that player. Use /notify_me to "
-      "receive a private message when a new game is started.\n\n"
-      "<b>Language</b> and other settings: /settings\n"
-      "Other commands (only game creator):\n"
-      "/close - Close lobby\n"
-      "/open - Open lobby\n"
-      "/kill - Terminate the game\n"
-      "/kick - Select a player to kick "
+    help_text = _("HALO AKU ADALAH UNO GAME BOT 🤖\n"
+      " Jika ingin memainkan Bot ini silahkan ikuti langkah dibawah ini :\n\n"
+      "** 1. Tambahkan bot ini ke grup **\n"
+      "** 2. Di grup, mulai permainan baru dengan /new **"
+      "** Jangan lupa bergabung ke lobi, klik : /join **\n"
+      "** 3. Setelah setidaknya dua pemain bergabung, mulailah permainan dengan **"
+      "** Menekan /start **\n"
+      "📑 __ Perintah Lainnya : __ /n"
+      " /Leave : Untuk meninggalkan permainan, "
+      " /skip  : untuk melewati pemain lain "
+      " /settings : Untuk Melihat pengaturan group"
+      "📚 Perintah lainnya (Hanya untuk pembuat game):\n"
+      "/close : Tutup lobi\n"
+      "/open : Buka lobi\n"
+      "/kill : Hentikan permainan\n"
+      "/kick : Pilih pemain untuk ditendang "
       "by replying to him or her\n"
-      "/enable_translations - Translate relevant texts into all "
-      "languages spoken in a game\n"
-      "/disable_translations - Use English for those texts\n\n"
-      "<b>Experimental:</b> Play in multiple groups at the same time. "
-      "Press the <code>Current game: ...</code> button and select the "
-      "group you want to play a card in.\n"
-      "If you enjoy this bot, "
-      "<a href=\"https://telegram.me/storebot?start=mau_mau_bot\">"
-      "rate me</a>, join the "
-      "<a href=\"https://t.me/rexaprivateroom\">update channel</a>"
-      " and buy an UNO card game.")
+      "/enable_translations : Terjemahkan teks yang relevan ke semua "
+      "/disable_translations - Gunakan bahasa Inggris untuk teks tersebut\n\n"
+      "👨‍💻 About : "
+      "<a href=\"https://t.me/rexaprivateroom\">👥 update group</a>"
+      "<a href=\"https://t.me/tirexgugel\">🚀 update channel</a>"
+      "<a href=\"https://t.me/JustRex\">👨‍💻 Owner</a>"
+      " Selamat Bermain :).")
 
     send_async(context.bot, update.message.chat_id, text=help_text,
                parse_mode=ParseMode.HTML, disable_web_page_preview=True)
@@ -83,16 +72,16 @@ def modes(update: Update, context: CallbackContext):
 @user_locale
 def source(update: Update, context: CallbackContext):
     """Handler for the /help command"""
-    source_text = _("This bot is Free Software and licensed under the AGPL. "
-      "The code is available here: \n"
+    source_text = _("Bot ini adalah Hasil Kang dari Mau Mau uno bot saya hanya merubah sedikit tampilan. "
+      "Kode tersedia di sini: \n"
       "https://github.com/jh0ker/mau_mau_bot")
     attributions = _("Attributions:\n"
       'Draw icon by '
       '<a href="http://www.faithtoken.com/">Faithtoken</a>\n'
       'Pass icon by '
       '<a href="http://delapouite.com/">Delapouite</a>\n'
-      "Originals available on http://game-icons.net\n"
-      "Icons edited by ɳick")
+      "Asli tersedia di http://game-icons.net\n"
+      "Ikon diedit oleh ɳick")
 
     send_async(context.bot, update.message.chat_id, text=source_text + '\n' +
                                                  attributions,
